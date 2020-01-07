79249f8bd4f21caa37854be5e73725bd5880d449
Mattias <ma10s@hotmail.com>
2020-01-07 14:52:37 +0100

    updated the ML_model to a class

--- load_data.py
+++ load_data.py
@@ -2,34 +2,62 @@ import os
 import numpy as np
 from PIL import Image
 from matplotlib import pylab as plt
+from scipy import misc
+from utilities import split_train_test_set
 
 
+class_keys = {'BAS': 0, 'EBO': 1, 'EOS': 2, 'KSC': 3, 'LYA': 4, 'LYT': 5, 'MMZ': 6, 'MOB': 7, 'MON': 8, 'MYB': 9 , 'MYO': 10,
+        'NGB': 11, 'NGS': 12, 'PMB': 13, 'PMO': 14}
 
 main_folder = "AML-Cytomorphology_LMU"
 
-def load_data(roof=100):
+def load_data(roof=100,downsample=False):
 
+    first = True
     image_data = []
     class_data = []
-    class_index = 0
+    #class_index = 0
     for filename in os.listdir(main_folder):
+        class_index = class_keys[filename]
+        print("class index: ", class_index)
         nr = 0
         for image in os.listdir(main_folder + "/" + filename):
             nr += 1
             im = Image.open(main_folder + "/" + filename + "/" + image)
             np_image = np.array(im)
+            np_image = misc.imresize(np_image, 0.25)
             image_data += [np_image]
             class_data += [class_index]
             if roof:
                 if nr >= roof:
                     break
+            if first:
+                plt.imshow(np_image)
+                plt.show()
+                print("np_image shape: ", np_image.shape)
+
+                first = False
 
         print(filename, " got ", nr, " sets.")
-        class_index += 1
+        #class_index += 1
 
     return np.array(image_data), np.array(class_data)
 
 
-# image_data, class_data = load_data()
+# image_data, class_data = load_data(roof=None)
 # print("image data shape: ", image_data.shape)
 # print("class data shape: ", class_data.shape)
+#
+# train_x, train_y, test_x, test_y = split_train_test_set(image_data, class_data, split=0.1)
+#
+# def check_class_nr(class_data):
+#
+#     classes, return_counts = np.unique(class_data, return_counts = True)
+#     for i in range(len(classes)):
+#         print("class ", class_keys[classes[i]], ": ", return_counts[i])
+#
+#
+# print("train check: ")
+# check_class_nr(train_y)
+# print("test check: ")
+# check_class_nr(test_y)
--- utilities.py
+++ utilities.py
@@ -7,3 +7,40 @@ def one_hot(class_data, classes=16):
     ret = np.zeros((len(class_data),classes))
     ret[np.arange(len(class_data)),class_data] = 1
     return ret
+
+
+def split_train_test_set(image_data, class_data, split=0.1):
+
+    classes = np.unique(class_data)
+    class_ind = [[]]
+    i=0
+    for c in classes:
+        print("c: ", c)
+        class_ind[i] = np.where(class_data == c)
+        i += 1
+
+    train_x = []
+    train_y = []
+    test_x = []
+    test_y = []
+
+    for i in range(len(classes)):
+
+        split_i = int(len(class_ind[i][0])*(1-split))
+        if i == 0:
+            train_x = image_data[:split_i]
+            train_y = class_data[:split_i]
+            test_x = image_data[split_i:]
+            test_y = class_data[split_i:]
+        else:
+            train_x += image_data[:split_i]
+            train_y += class_data[:split_i]
+            test_x += image_data[split_i:]
+            test_y += class_data[split_i:]
+        
+    return np.array(train_x), np.array(train_y), np.array(test_x), np.array(test_y)
+            
+            
+        
+
+
